d84ca32214795090ef511b7d2687366d6d30bf79
Brandon Dith-Berry <brandondithberry@gmail.com>
2022-03-19 21:57:21 -0500

    Settings Update

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,23 @@
 <template>
-  <section></section>
+  <section>
+    <h1>{{ about.content.title }}</h1>
+    <p>{{ about.content.intro }}</p>
+  </section>
 </template>
 
 <style lang="postcss" scoped>
 </style>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content, error }) {
+    let about
+    try {
+      about = await $content('about').fetch()
+    } catch (e) {
+      error({ message: 'About not found' })
+    }
+    return { about }
+  },
+}
 </script>
