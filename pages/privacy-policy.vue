bd842054837a56ced12b65d10c4af49cf0306dd6
Brandon Dith-Berry <brandondithberry@gmail.com>
2022-03-31 13:56:47 -0500

    Adjust to MD

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,19 +1,14 @@
 <template>
   <section>
     <h1>Privacy Policy</h1>
-    <nuxt-content :document="policy" />
+    <nuxt-content :document="policy.content"></nuxt-content>
   </section>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      policy: [],
-    }
-  },
-  async fetch({ $content, params }) {
-    const policy = await $content('policies/privacy-policy', params.content).fetch()
+  async asyncData({ $content }) {
+    const policy = await $content('policies/privacy-policy').fetch()
 
     return {
       policy,
